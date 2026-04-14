a1bb77c18f57e01809f913644488af77305c8d95
aleveha <nikita.tashilin@gmail.com>
2026-04-13 20:16:45 -0400

    chore: add clsx and tailwind-merge, add cn utility function for class merging

---
+++ src/lib/cn.ts
@@ -0,0 +1,6 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+type ClassValue = Parameters<typeof clsx>[0];
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
