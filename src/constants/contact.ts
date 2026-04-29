378048c5c047c3fba81664fe08d5fcec13208c1a
aleveha <nikita.tashilin@gmail.com>
2026-04-29 12:20:34 -0400

    feat: centralize social media links in constants

---
+++ src/constants/contact.ts
@@ -0,0 +1,11 @@
+export const INSTAGRAM_URL =
+	"https://www.instagram.com/aidenliamframeprotection";
+
+export const FACEBOOK_URL =
+	"https://www.facebook.com/aidenliamframeprotectioninstalls";
+
+export const WHATSAPP_URL = "https://wa.me/14375550142";
+export const WHATSAPP_DISPLAY = "+1 (437) 555-0142";
+
+export const EMAIL_URL = "mailto:contact@your-domain.ca";
+export const EMAIL_DISPLAY = "contact@your-domain.ca";
